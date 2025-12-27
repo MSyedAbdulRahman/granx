04aad30b3c136e88997ae426e27bbcfd52439e04
MSyedAbdulRahman <syedabdulrahman@gmail.com>
2025-12-27 14:02:13 +0530

    OLD File

---
+++ services.php
@@ -0,0 +1,90 @@
+<?php include "header.php" ?>
+
+		<!-- Start Breadcrumb
+		============================================= -->
+		<div class="site-breadcrumb" style="background: url(assets/images/granax-banner.jpg)">
+			<div class="container">
+				<div class="site-breadcrumb-wpr">
+					<h2 class="breadcrumb-title">SERVICES</h2>
+					<ul class="breadcrumb-menu clearfix">
+						<li><a href="index.php">HOME</a></li>
+						<li class="active">Services</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+        <!-- End Breadcrumb -->
+
+<!-- Start Service Single
+		============================================= -->
+<div class="service-single pos-rel de-padding">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-xl-4">
+                <aside class="sidebar">
+                    <!-- Services -->
+                    <div class="widget service-klo p-40-30 category">
+                        <div class="service-category-list">
+                            <h4 class="ttt mb-20">Services</h4>
+                            <ul class="service-sl-list">
+                                <li>
+                                    <a href="custom-compounding.php">
+                                        <i class="fa-solid fa-circle"></i>
+                                        <span>Custom Compounding</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="toll-compounding.php">
+                                        <i class="fa-solid fa-circle"></i>
+                                        <span>Toll Compounding</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="recycled-compounding.php">
+                                        <i class="fa-solid fa-circle"></i>
+                                        <span>Recycled Compounds</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="technical-support.php">
+                                        <i class="fa-solid fa-circle"></i>
+                                        <span>Technical Support</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="color-compounding.php">
+                                        <i class="fa-solid fa-circle"></i>
+                                        <span>Color Compounding</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="nano-composite.php">
+                                        <i class="fa-solid fa-circle"></i>
+                                        <span>Nano Composite</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </aside>
+            </div>
+            <div class="col-xl-8">
+                <div class="service-single-wpr pl-60">
+                    <h2 class="heading-2" style="background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
+    -webkit-text-fill-color: transparent;">
+                        CUSTOM COMPOUNDING
+                    </h2>
+                    <img src="assets/images/services/custom-compounding.png" class="d-block mb-40" alt="no image">
+                    <p class="service-slg-tt mb-30">
+                        We are focused on developing custom compounds and formulations for the various specialty applications of Injection moulding customers for industrial and commercial purposes. We make it our business to meet the custom requirements of our customers with every application.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Service Single -->
+
+<?php include "footer.php" ?>
+<?php include "script.php" ?>
+<?php include "bottom.php" ?>
