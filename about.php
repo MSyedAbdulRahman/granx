dc76f1a12861f72541e2630eb336394774d1c395
MSyedAbdulRahman <syedabdulrahman@gmail.com>
2025-12-27 13:47:27 +0530

    OLD File

---
+++ about.php
@@ -0,0 +1,94 @@
+<?php include "header.php" ?>
+
+	<main class="main">
+        
+		<!-- Start Breadcrumb
+		============================================= -->
+		<div class="site-breadcrumb" style="background: url(assets/images/granax-banner.jpg)">
+			<div class="container">
+				<div class="site-breadcrumb-wpr">
+					<h2 class="breadcrumb-title">ABOUT US</h2>
+					<ul class="breadcrumb-menu clearfix">
+						<li><a href="index.php">HOME</a></li>
+						<li class="active">About Us</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+        <!-- End Breadcrumb -->
+		
+		<!-- Start About us
+		============================================= -->
+		<div class="about-area de-padding">
+			<div class="container">
+				<div class="about-wpr grid-2">
+					<div class="about-left">
+						<div class="about-left-pix pos-rel wow animate__animated animate__fadeInLeft"
+							data-wow-duration="1s" data-wow-offset="100">
+							<img src="assets/images/nano-about.png"
+								class="about-1-1 wow animate__animated animate__zoomIn" alt="no thumb"
+								data-wow-delay=".15s" data-wow-duration="1s">
+							<img src="assets/images/nano-about-1.png"
+								class="about-2-1 wow animate__animated animate__fadeInUp" alt="no thumb"
+								data-wow-delay=".3s" data-wow-duration="1s">
+							<div class="about-opt-exp wow animate__animated animate__bounceIn" data-wow-delay=".45s"
+								data-wow-duration=".9s">
+								<h3 class="num">25+</h3>
+								<p class="mb-0">Years Experience</p>
+							</div>
+							<div class="about-opt-dots wow animate__animated animate__fadeIn" data-wow-delay=".6s"
+								data-wow-duration="1s"></div>
+						</div>
+					</div>
+					<div class="about-right">
+						<div class="status-btn mb-20 wow animate__animated animate__fadeInDown" data-wow-delay=".1s"
+							data-wow-duration=".8s">
+							About Us
+							<span class="dot"></span>
+						</div>
+						<h2 class="hero-title mb-30 wow animate__animated animate__fadeInUp" data-wow-delay=".2s"
+							data-wow-duration="1s"
+							style="font-weight: 600;
+							font-size: 40px;    background: -webkit-linear-gradient(138deg, #01bc25, #4673a2); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"> Granx - Nano Polymers
+						</h2>
+						<p class="mb-40 wow animate__animated animate__fadeInUp" data-wow-delay=".35s"
+							data-wow-duration="1s" style="font-size: 20px;text-align: justify;" id="aboutus">
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End About us -->
+
+		<br>
+		<!--Start Vision and Mission-->
+		<div class="gmc-body" bis_skin_checked="1">
+			<div class="gmc-box gmc-visible" bis_skin_checked="1">
+				<div class="gmc-icon" bis_skin_checked="1">
+					<img src="assets/gif/eye-target.gif" alt="Vision Icon">
+				</div>
+				<h3 class="gmc-title" style="font-weight: 600;
+				font-size: 25px;
+				background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
+				-webkit-text-fill-color: transparent;">Vision</h3>
+				<p style="text-align: justify;" id="vision"></p>
+			</div>
+
+			<div class="gmc-box gmc-visible" bis_skin_checked="1">
+				<div class="gmc-icon" bis_skin_checked="1">
+					<img src="assets/gif/mission.gif" alt="Mission Icon">
+				</div>
+				<h3 class="gmc-title" style="font-weight: 600;
+				font-size: 25px;
+				background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
+				-webkit-text-fill-color: transparent;">Mission</h3>
+				<p style="text-align: justify;" id="mission"></p>
+			</div>
+		</div>
+		<!-- End Vision and Mission -->
+		
+	</main>	
+
+<?php include "footer.php" ?>
+<?php include "script.php" ?>
+<?php include "bottom.php" ?>
