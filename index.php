be9f938fdb8e90f919d865dbfd08bceea265dd54
MSyedAbdulRahman <syedabdulrahman@gmail.com>
2025-12-27 13:06:50 +0530

    OLD File index

---
+++ index.php
@@ -0,0 +1,371 @@
+<?php include "header.php" ?>
+
+	<main class="main">
+
+
+		<!-- Start Slider
+		============================================= -->
+		<div class="hero-section">
+
+			<!-- BACKGROUND VIDEO -->
+			<div class="hero-video">
+				<video src="assets/videos/video2.mp4" autoplay muted loop></video>
+			</div>
+
+			<!-- OVERLAY COLOR -->
+			<div class="video-overlay"></div>
+
+			<!-- SLICK OVERLAY SLIDER -->
+			<div class="overlay-slider slider">
+				<div class="slide-item">
+					<h2 class="slide-title" style="color: white;">Turning Waste into Value</h2>
+					<p class="slide-text">We recycle and re-compound plastics to give them a second life.</p>
+					<a href="about.php" class="read-more-btn">Read More</a>
+				</div>
+
+				<div class="slide-item">
+					<h2 class="slide-title" style="color: white;">Sustainable Plastic Recycling</h2>
+					<p class="slide-text">Committed to responsible recycling — for a cleaner planet & better future.</p>
+					<a href="about.php" class="read-more-btn">Read More</a>
+				</div>
+
+				<div class="slide-item">
+					<h2 class="slide-title" style="color: white;">Eco-Friendly Solutions for Industry</h2>
+					<p class="slide-text">Custom formulations, tested quality, dependable supply.</p>
+					<a href="about.php" class="read-more-btn">Read More</a>
+				</div>
+			</div>
+		</div>
+		<!-- End Slider -->
+
+		<!-- Start About us
+		============================================= -->
+		<div class="about-area de-padding">
+			<div class="container">
+				<div class="about-wpr grid-2">
+					<div class="about-left">
+						<div class="about-left-pix pos-rel wow animate__animated animate__fadeInLeft"
+							data-wow-duration="1s" data-wow-offset="100">
+							<img src="assets/images/nano-about.png"
+								class="about-1-1 wow animate__animated animate__zoomIn" alt="no thumb"
+								data-wow-delay=".15s" data-wow-duration="1s">
+							<img src="assets/images/nano-about-1.png"
+								class="about-2-1 wow animate__animated animate__fadeInUp" alt="no thumb"
+								data-wow-delay=".3s" data-wow-duration="1s">
+							<div class="about-opt-exp wow animate__animated animate__bounceIn" data-wow-delay=".45s"
+								data-wow-duration=".9s">
+								<h3 class="num">25+</h3>
+								<p class="mb-0">Years Experience</p>
+							</div>
+							<div class="about-opt-dots wow animate__animated animate__fadeIn" data-wow-delay=".6s"
+								data-wow-duration="1s"></div>
+						</div>
+					</div>
+					<div class="about-right">
+						<div class="status-btn mb-20 wow animate__animated animate__fadeInDown" data-wow-delay=".1s"
+							data-wow-duration=".8s">
+							About Us
+							<span class="dot"></span>
+						</div>
+						<h2 class="hero-title mb-30 wow animate__animated animate__fadeInUp" data-wow-delay=".2s"
+							data-wow-duration="1s"
+							style="font-weight: 600;
+							font-size: 40px;    background: -webkit-linear-gradient(138deg, #01bc25, #4673a2); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
+							Welcome to Granx - Nano Polymers
+						</h2>
+						<p class="mb-40 wow animate__animated animate__fadeInUp" data-wow-delay=".35s"
+							data-wow-duration="1s" style="font-size: 20px;text-align: justify;">
+							NanoPolymers is committed to custom compounding and toll compounding of Engineering Plastic
+							materials .Continuous research and advance processes have made it possible to use almost
+							every kind of engineering plastics all over again like virgin materials. We have modified
+							Nylons , Polycarbonates , ABS and PP for our customers.
+							<br>
+							<br>
+							We have Twin screw extruders which are technically better than kneader or single screw
+							extruders for compounding needs We can have better dispersion of additives and colours
+							compounding and needs.
+							<br>
+							<br>
+							NanoPolymers will be the plastic compounding and recycling company, determined to recycle
+							all plastics through awareness and individual responsibility. Nanopolymers is a premier
+							supplier of compounded plastics and custom formulations. We strive to meet and exceed
+							customer expectations every time. For us, excellence is more than a goal, it’s a way of
+							doing business.
+						</p>
+						<a href="about.php" class="btn-1 btn-md wow animate__animated animate__fadeInUp" data-wow-delay=".85s"
+							data-wow-duration="1s" style="background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37))">
+							Read More
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End About us -->
+
+		<br>
+		<!--Start Vision and Mission-->
+		<div class="gmc-body" bis_skin_checked="1">
+			<div class="gmc-box gmc-visible" bis_skin_checked="1">
+				<div class="gmc-icon" bis_skin_checked="1">
+					<img src="assets/gif/eye-target.gif" alt="Vision Icon">
+				</div>
+				<h3 class="gmc-title" style="font-weight: 600;
+				font-size: 25px;
+				background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
+				-webkit-text-fill-color: transparent;">Vision</h3>
+				<p style="text-align: justify;">We want to make a better world were the waste will become the new raw
+					materials and give a new life
+					for every object we decided to dispense.
+					Plastic doesn’t break down easily, if ever. Recycling plastic means that it is still plastic, just
+					being used for a different purpose. Therefore, we’re not actually reducing plastic amounts or
+					exposure through recycling but we keep that from increasing, spreading and polluting.</p>
+			</div>
+
+			<div class="gmc-box gmc-visible" bis_skin_checked="1">
+				<div class="gmc-icon" bis_skin_checked="1">
+					<img src="assets/gif/mission.gif" alt="Mission Icon">
+				</div>
+				<h3 class="gmc-title" style="font-weight: 600;
+				font-size: 25px;
+				background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
+				-webkit-text-fill-color: transparent;">Mission</h3>
+				<p style="text-align: justify;">Our aim is to not only transpire as a leading manufacturer and supplier
+					of prime quality plastic
+					compounding and recycled granules but also create a proper brand image among its target audience and
+					the market with effective service continuum.</p>
+			</div>
+		</div>
+		<!-- End Vision and Mission -->
+
+		<!--Start Service-->
+		<div class="blog-area de-padding" bis_skin_checked="1">
+			<div class="container" bis_skin_checked="1">
+				<div class="row" bis_skin_checked="1">
+					<div class="col-xl-12" bis_skin_checked="1">
+						<div class="site-title text-center" bis_skin_checked="1">
+							<div class="status-btn mb-20 wow animate__ animate__fadeInDown animated"
+								data-wow-delay=".05s" data-wow-duration=".8s" bis_skin_checked="1"
+								style="visibility: visible; animation-duration: 0.8s; animation-delay: 0.05s; animation-name: fadeInDown;">
+								<span class="dot"></span>
+								Our Services
+							</div>
+							<h2 class="mb-0 wow animate__ animate__fadeInUp animated" data-wow-delay=".15s"
+								data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.15s; animation-name: fadeInUp;background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
+							-webkit-text-fill-color: transparent;    font-size: 40px;">
+								Creative Projects for <br>
+								Every Business Goal
+							</h2>
+						</div>
+					</div>
+				</div>
+				<div class="blog-wpr grid-3" bis_skin_checked="1">
+					<div class="blog-box" bis_skin_checked="1">
+						<a href="custom-compounding.php">
+							<img src="assets/images/custom-compounding.png" alt="no image">
+						</a>
+						<div class="blog-desc pos-rel" bis_skin_checked="1" style="background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37))">
+							<h4 class="blog-title mb-0">
+								<a href="custom-compounding.php" style=" color: #e4ff2a;">
+									CUSTOM COMPOUNDING
+								</a>
+								<p style="color: white;font-size:18px;padding-top:10px">We are focused on developing
+									custom compounds and formulations for the various specialty applications </p>
+							</h4>
+							<a href="custom-compounding.php" class="blog-btn-pos">
+								<i>
+									<svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M12.5207 1.3934C12.5207 0.841114 12.0729 0.393398 11.5207 0.393398H2.52066C1.96838 0.393398 1.52066 0.841114 1.52066 1.3934C1.52066 1.94568 1.96838 2.3934 2.52066 2.3934H10.5207V10.3934C10.5207 10.9457 10.9684 11.3934 11.5207 11.3934C12.0729 11.3934 12.5207 10.9457 12.5207 10.3934V1.3934ZM1.62117 12.7071L12.2278 2.10051L10.8136 0.686292L0.206956 11.2929L1.62117 12.7071Z">
+										</path>
+									</svg>
+								</i>
+							</a>
+						</div>
+					</div>
+					<div class="blog-box" bis_skin_checked="1">
+						<a href="toll-compounding.php">
+							<img src="assets/images/toll-compunding.png" alt="no image">
+						</a>
+						<div class="blog-desc pos-rel" bis_skin_checked="1" style="background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37))">
+							<h4 class="blog-title mb-0">
+								<a href="toll-compounding.php" style=" color: #e4ff2a;">
+									TOLL COMPOUNDING
+								</a>
+								<p style="color: white;font-size:18px;padding-top:10px">Nano Polymers offers Toll
+									Compounding services for customers who have developed a compound formulation </p>
+							</h4>
+							<a href="toll-compounding.php" class="blog-btn-pos">
+								<i>
+									<svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M12.5207 1.3934C12.5207 0.841114 12.0729 0.393398 11.5207 0.393398H2.52066C1.96838 0.393398 1.52066 0.841114 1.52066 1.3934C1.52066 1.94568 1.96838 2.3934 2.52066 2.3934H10.5207V10.3934C10.5207 10.9457 10.9684 11.3934 11.5207 11.3934C12.0729 11.3934 12.5207 10.9457 12.5207 10.3934V1.3934ZM1.62117 12.7071L12.2278 2.10051L10.8136 0.686292L0.206956 11.2929L1.62117 12.7071Z">
+										</path>
+									</svg>
+								</i>
+							</a>
+						</div>
+					</div>
+					<div class="blog-box" bis_skin_checked="1">
+						<a href="recycled-compounding.php">
+							<img src="assets/images/recycled-coumpoundes.png" alt="no image">
+						</a>
+						<div class="blog-desc pos-rel" bis_skin_checked="1" style="background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37))">
+
+							<h4 class="blog-title mb-0">
+								<a href="recycled-compounding.php" style=" color: #e4ff2a;">
+									RECYCLED COMPOUNDS
+								</a>
+								<p style="color: white;font-size:18px;padding-top:10px">Recycled Compounds allows
+									manufacturers to recover and reuse production scrap and waste by outsourcing </p>
+							</h4>
+							<a href="recycled-compounding.php" class="blog-btn-pos">
+								<i>
+									<svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M12.5207 1.3934C12.5207 0.841114 12.0729 0.393398 11.5207 0.393398H2.52066C1.96838 0.393398 1.52066 0.841114 1.52066 1.3934C1.52066 1.94568 1.96838 2.3934 2.52066 2.3934H10.5207V10.3934C10.5207 10.9457 10.9684 11.3934 11.5207 11.3934C12.0729 11.3934 12.5207 10.9457 12.5207 10.3934V1.3934ZM1.62117 12.7071L12.2278 2.10051L10.8136 0.686292L0.206956 11.2929L1.62117 12.7071Z">
+										</path>
+									</svg>
+								</i>
+							</a>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class="blog-wpr grid-3" bis_skin_checked="1">
+					<div class="blog-box" bis_skin_checked="1">
+						<a href="technical-support.php">
+							<img src="assets/images/technical-support.png" alt="no image">
+						</a>
+						<div class="blog-desc pos-rel" bis_skin_checked="1" style="background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37))">
+							<h4 class="blog-title mb-0">
+								<a href="technical-support.php" style=" color: #e4ff2a;">
+									TECHNICAL SUPPORT
+								</a>
+								<p style="color: white;font-size:18px;padding-top:10px">We provide quality compounding products and the technical support that gives you confidence </p>
+							</h4>
+							<a href="technical-support.php" class="blog-btn-pos">
+								<i>
+									<svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M12.5207 1.3934C12.5207 0.841114 12.0729 0.393398 11.5207 0.393398H2.52066C1.96838 0.393398 1.52066 0.841114 1.52066 1.3934C1.52066 1.94568 1.96838 2.3934 2.52066 2.3934H10.5207V10.3934C10.5207 10.9457 10.9684 11.3934 11.5207 11.3934C12.0729 11.3934 12.5207 10.9457 12.5207 10.3934V1.3934ZM1.62117 12.7071L12.2278 2.10051L10.8136 0.686292L0.206956 11.2929L1.62117 12.7071Z">
+										</path>
+									</svg>
+								</i>
+							</a>
+						</div>
+					</div>
+					<div class="blog-box" bis_skin_checked="1">
+						<a href="color-compounding.php">
+							<img src="assets/images/color-compounding.png" alt="no image">
+						</a>
+						<div class="blog-desc pos-rel" bis_skin_checked="1" style="background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37))">
+							<h4 class="blog-title mb-0">
+								<a href="color-compounding.php" style=" color: #e4ff2a;">
+									COLOR COMPOUNDING
+								</a>
+								<p style="color: white;font-size:18px;padding-top:10px">Custom-Coloured or additive masterbatch formulation and compounding. That means not just “custom compounds”</p>
+							</h4>
+							<a href="color-compounding.php" class="blog-btn-pos">
+								<i>
+									<svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M12.5207 1.3934C12.5207 0.841114 12.0729 0.393398 11.5207 0.393398H2.52066C1.96838 0.393398 1.52066 0.841114 1.52066 1.3934C1.52066 1.94568 1.96838 2.3934 2.52066 2.3934H10.5207V10.3934C10.5207 10.9457 10.9684 11.3934 11.5207 11.3934C12.0729 11.3934 12.5207 10.9457 12.5207 10.3934V1.3934ZM1.62117 12.7071L12.2278 2.10051L10.8136 0.686292L0.206956 11.2929L1.62117 12.7071Z">
+										</path>
+									</svg>
+								</i>
+							</a>
+						</div>
+					</div>
+					<div class="blog-box" bis_skin_checked="1">
+						<a href="nano-composite.php">
+							<img src="assets/images/nano-composite.png" alt="no image">
+						</a>
+						<div class="blog-desc pos-rel" bis_skin_checked="1" style="background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37))">
+							<h4 class="blog-title mb-0">
+								<a href="nano-composite.php" style=" color: #e4ff2a;">
+									NANO COMPOSITE
+								</a>
+								<p style="color: white;font-size:18px;padding-top:10px">compounding with nano- or specialty-additives, e.g. nanoclays, fibers, fillers, flame retardants </p>
+							</h4>
+							<a href="nano-composite.php" class="blog-btn-pos">
+								<i>
+									<svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M12.5207 1.3934C12.5207 0.841114 12.0729 0.393398 11.5207 0.393398H2.52066C1.96838 0.393398 1.52066 0.841114 1.52066 1.3934C1.52066 1.94568 1.96838 2.3934 2.52066 2.3934H10.5207V10.3934C10.5207 10.9457 10.9684 11.3934 11.5207 11.3934C12.0729 11.3934 12.5207 10.9457 12.5207 10.3934V1.3934ZM1.62117 12.7071L12.2278 2.10051L10.8136 0.686292L0.206956 11.2929L1.62117 12.7071Z">
+										</path>
+									</svg>
+								</i>
+							</a>
+						</div>
+					</div>
+				</div>
+				<br>
+				<!-- <center>
+					<a href="services.php" class="btn-1 btn-md wow animate__ animate__fadeInUp animated" data-wow-delay=".85s"
+						data-wow-duration="1s"
+						style="visibility: visible; animation-duration: 1s; animation-delay: 0.85s; animation-name: fadeInUp;background: linear-gradient(116deg, rgb(14 80 149), rgb(1, 188, 37));">
+						View More
+					</a>
+				</center> -->
+			</div>
+		</div>
+		<!--End Service-->
+
+		<!--Start Team-->
+		<center>
+			<div class="team-area bg-black-4 pos-rel de-padding" bis_skin_checked="1" style="
+			background: linear-gradient(90deg, #0442c6, #00bc24);padding: 15px;
+		">
+				<img src="assets/img/vector/team-shape.png" class="team-shape" alt="no image">
+				<div class="container" bis_skin_checked="1">
+					<div class="row" bis_skin_checked="1">
+						<div class="col-xl-12" bis_skin_checked="1">
+							<div class="site-title wh text-center" bis_skin_checked="1">
+								<div class="status-btn mb-20 wow animate__ animate__fadeInDown animated"
+									data-wow-delay=".05s" data-wow-duration=".8s" bis_skin_checked="1"
+									style="visibility: visible; animation-duration: 0.8s; animation-delay: 0.05s; animation-name: fadeInDown;">
+									<span class="dot"></span>
+									Our Experts
+								</div>
+								<h2 class="mb-0 wow animate__ animate__fadeInUp animated" data-wow-delay=".15s"
+									data-wow-duration="1s"
+									style="visibility: visible; animation-duration: 1s; animation-delay: 0.15s; animation-name: fadeInUp;font-size: 35px;">
+									The People Behind Our Success <br> and Future Growth
+								</h2>
+							</div>
+						</div>
+					</div>
+
+					<div class="team-wpr grid-3 grid-new" bis_skin_checked="1">
+						<div class="team-area-card wow animate__ animate__fadeInUp animated" data-wow-delay=".1s"
+							data-wow-duration="1s" bis_skin_checked="1"
+							style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeInUp;">
+							<div class="team-area-img" bis_skin_checked="1">
+								<img src="assets/images/no-image.png" alt="no thumb">
+							</div>
+							<h3 class="team-area-name">M.Syed Sulaiman PDPMT Cipet</h3>
+							<p class="team-area-role" style="font-size: 25px;
+							color: yellow;">Managing Partner</p>
+						</div>
+
+						<div class="team-area-card wow animate__ animate__fadeInUp animated" data-wow-delay=".2s"
+							data-wow-duration="1s" bis_skin_checked="1"
+							style="visibility: visible; animation-duration: 1s; animation-delay: 0.2s; animation-name: fadeInUp;">
+							<div class="team-area-img" bis_skin_checked="1">
+								<img src="assets/images/no-image.png" alt="no thumb">
+							</div>
+							<h3 class="team-area-name">M.Aravinth Kumar PDPMT Cipet</h3>
+							<p class="team-area-role" style="font-size: 25px;
+							color: yellow;">Managing Partner</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</center>
+
+		<!--End Team-->
+	</main>
+
+<?php include "footer.php" ?>
+<?php include "script.php" ?>
+<?php include "bottom.php" ?>
