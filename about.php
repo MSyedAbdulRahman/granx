154a27a451b4c277ace265e3ed88a61cf9df53f7
MSyedAbdulRahman <syedabdulrahman@gmail.com>
2025-12-27 13:56:26 +0530

    UPDATE: Vision and Mission

--- about.php
+++ about.php
@@ -19,7 +19,7 @@
 		
 		<!-- Start About us
 		============================================= -->
-		<div class="about-area de-padding">
+		<div class="about-area de-padding mb-granx">
 			<div class="container">
 				<div class="about-wpr grid-2">
 					<div class="about-left">
@@ -49,17 +49,22 @@
 						<h2 class="hero-title mb-30 wow animate__animated animate__fadeInUp" data-wow-delay=".2s"
 							data-wow-duration="1s"
 							style="font-weight: 600;
-							font-size: 40px;    background: -webkit-linear-gradient(138deg, #01bc25, #4673a2); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"> Granx - Nano Polymers
+							font-size: 40px;    background: -webkit-linear-gradient(138deg, #01bc25, #4673a2); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
+							Welcome to Granx - Nano Polymers
 						</h2>
 						<p class="mb-40 wow animate__animated animate__fadeInUp" data-wow-delay=".35s"
-							data-wow-duration="1s" style="font-size: 20px;text-align: justify;" id="aboutus">
+							data-wow-duration="1s" style="font-size: 20px;text-align: justify;">
+							NANOPOLYMERS is a technology-focused polymer compounding company engaged in the development of engineering plastic compounds, professional toll compounding services, and value-added recycled materials.
+							<br>
+							<br>
+							Operating through our flagship brand GRANX, we manufacture production-ready polymer compounds for automotive, electrical & electronics, and furniture component applications. By combining material science expertise with controlled processes, we provide consistent, application-driven polymer solutions tailored to demanding industrial environments
 						</p>
 					</div>
 				</div>
 			</div>
 		</div>
 		<!-- End About us -->
-
+	<br><br>
 		<br>
 		<!--Start Vision and Mission-->
 		<div class="gmc-body" bis_skin_checked="1">
@@ -71,7 +76,7 @@
 				font-size: 25px;
 				background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
 				-webkit-text-fill-color: transparent;">Vision</h3>
-				<p style="text-align: justify;" id="vision"></p>
+				<p style="text-align: justify;">To be the industry benchmark in redefined-performance compounding, value-added recycling, and reliable tolling</p>
 			</div>
 
 			<div class="gmc-box gmc-visible" bis_skin_checked="1">
@@ -82,7 +87,7 @@
 				font-size: 25px;
 				background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text;
 				-webkit-text-fill-color: transparent;">Mission</h3>
-				<p style="text-align: justify;" id="mission"></p>
+				<p style="text-align: justify;">To advance materials excellence through innovation, process control, and technical expertise—supporting customer success and long-term industrial growth.</p>
 			</div>
 		</div>
 		<!-- End Vision and Mission -->
