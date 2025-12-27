de10933b362834fb8a74c79c686e327234ed79fd
MSyedAbdulRahman <syedabdulrahman@gmail.com>
2025-12-27 14:17:00 +0530

    UPDATE : ALL CONTENT

---
+++ testing.php
@@ -0,0 +1,60 @@
+<?php include "header.php" ?>
+
+<!-- Start Breadcrumb
+		============================================= -->
+<div class="site-breadcrumb" style="background: url(assets/images/granax-banner.jpg)">
+    <div class="container">
+        <div class="site-breadcrumb-wpr">
+            <h2 class="breadcrumb-title">TESTING</h2>
+            <ul class="breadcrumb-menu clearfix">
+                <li><a href="index.php">HOME</a></li>
+                <li class="active">Testing</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End Breadcrumb -->
+
+<!-- Start Service Single
+		============================================= -->
+<div class="service-single pos-rel de-padding">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-xl-12">
+                <div class="service-single-wpr pl-60">
+                    <img src="assets/images/granax-testing.png" class="d-block mb-40" alt="no image">
+                    <h2 class="heading-2"
+                        style="background: -webkit-linear-gradient(138deg, rgb(1, 188, 37), rgb(70, 115, 162)) text; -webkit-text-fill-color: transparent;">
+                        Advanced Testing & Quality Control Laboratory
+                    </h2>
+                    <p class="service-slg-tt mb-30">
+                        Our in-house laboratory supports process validation, batch verification, and continuous quality
+                        monitoring, enabling reliable, repeatable quality for Engineered Polymer Compounds.
+                        Testing Equipment & Capabilities
+                    </p>
+                    <ul>
+                        <li>1. &nbsp;Bulk Density Apparatus</li>
+                        <li>2. &nbsp;Universal Testing Machine (UTM)</li>
+                        <li>3. &nbsp;Melt Flow Index Tester (MFI)</li>
+                        <li>4. &nbsp;Moisture Analyzer</li>
+                        <li>5. &nbsp;Filler / Ash Content Test Furnace</li>
+                        <li>6. &nbsp;Density Testing Kit</li>
+                        <li>7. &nbsp;Laboratory Balance </li>
+                        <li>8. &nbsp;Impact Testing Machine</li>
+                        <li>9. &nbsp;Izod notch cutter</li>
+                    </ul>
+                    <br>
+                    <p class="service-slg-tt mb-30">
+                        All testing activities follow defined SOP, ensuring traceability, data reliability, and
+                        batch-to-batch consistency.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Service Single -->
+
+<?php include "footer.php" ?>
+<?php include "script.php" ?>
+<?php include "bottom.php" ?>
