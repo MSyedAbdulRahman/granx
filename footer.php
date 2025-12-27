930d48e8945a447afa8ea3534408fc113a6bf2d4
MSyedAbdulRahman <syedabdulrahman@gmail.com>
2025-12-27 13:08:04 +0530

    OLD File footer

---
+++ footer.php
@@ -0,0 +1,114 @@
+<!-- Start Footer 
+	============================================= -->
+	<footer class="footer footer-1 bg-black-4 overflow-hidden pos-rel" style="background-image: url('assets/images/footer-bg.png');">
+		<!-- <img src="assets/img/vector/abs-3.png" class="abs-contact" alt="no image"> -->
+		<div class="container">
+			<div class="footer-wpr pos-rel">
+				<div class="footer-wpr-shape">
+				</div>
+				<div class="footer-wpr-down de-padding">
+					<div class="row footer-raw g-5">
+						<div class="col-xl-3 col-lg-6">
+							<div class="footer-about pl-60">
+								<h4 class="footer-heading text-white mb-20" style="color: yellow !important;text-align: left;">ABOUT US</h4>
+								<p class="mb-30" style="color: white;text-align: left;">
+									Nano Polymers is committed to custom compounding and toll compounding of Engineering. Plastic materials. Continuous research and advance processes have made it possible.
+								</p>
+								<ul class="footer-social" style="justify-content: left;">
+									<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+									<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+									<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-xl-2 col-lg-6">
+							<div class="footer-list-wpr">
+								<h4 class="footer-heading text-white mb-20" style="color: yellow !important;text-align: left;">QUICK LINKS</h4>
+								<ul class="footer-list" style="text-align: left;">
+									<li>
+										<a href="index.php" style="color: white;">
+											Home
+										</a>
+									</li>
+									<li>
+										<a href="about.php" style="color: white;">
+											About Us
+										</a>
+									</li>
+
+									<li>
+										<a href="services.php" style="color: white;">
+											Services
+										</a>
+									</li>
+									<li>
+										<a href="testing.php" style="color: white;">
+											Testing
+										</a>
+									</li>
+									<li>
+										<a href="contact.php" style="color: white;">
+											Contact Us
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-xl-3 col-lg-6">
+							<div class="footer-list-wpr">
+								<h4 class="footer-heading text-white mb-20" style="color: yellow !important;text-align: left;">SERVICES</h4>
+								<ul class="footer-list" style="text-align: left;">
+									<li>
+										<a href="custom-compounding.php" style="color: white;">
+											Custom Compounding
+										</a>
+									</li>
+
+									<li>
+										<a href="toll-compounding.php" style="color: white;">
+											Toll Compounding
+										</a>
+									</li>
+									<li>
+										<a href="recycled-compounding.php" style="color: white;">
+											Recycled Compounds
+										</a>
+									</li>
+									<li>
+										<a href="technical-support.php" style="color: white;">
+											Technical Support
+										</a>
+									</li>
+									<li>
+										<a href="color-compounding.php" style="color: white;">
+											Color Compounding
+										</a>
+									</li>
+									<li>
+										<a href="nano-composite.php" style="color: white;">
+											Nano Composite
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-xl-3 col-lg-6">
+							<h4 class="footer-heading text-white mb-20" style="color: yellow !important;">LOCATION</h4>
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3695.3393656915!2d80.09285497794066!3d12.991062054647216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a528aa98976f411%3A0xe47445ffc4cd958d!2sSubramaniya%20Swamy%20Nagar%2C%20Kundrathur%2C%20Chennai%2C%20Tamil%20Nadu%20600069!5e1!3m2!1sen!2sin!4v1764942039847!5m2!1sen!2sin" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright footer-wpr-bt py-5 grid-2">
+				<p class="mb-0" style="color: white;">© 2025 All rights reserved. Granx</p>
+				<span class="footer-social" style="justify-content: end;gap: 5px;color:white">Developed By<a href="https://archerwebsol.com/" target="_blank" style="color: yellow !important;">Archer Websol</a></span>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
+
+	<!-- Start Scroll top
+	============================================= -->
+	<a href="#bdy" id="scrtop" class="smooth-menu"><i class="ti-arrow-up"></i></a>
+	<!-- End Scroll top-->
